b68f91d47a53e37171281effe3fb9db7d76773cc
jonathanboris <ouedraogojonathanboris@gmail.com>
2023-05-09 22:18:27 +0200

    init template

--- fbapp/models.py
+++ fbapp/models.py
@@ -2,15 +2,22 @@
 from .views import app
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
+import enum
 
 # Create database connection object
 db = SQLAlchemy(app)
 
 
+class Gender(enum.Enum):
+    MALE = 0
+    FEMALE = 1
+    OTHER = 3
+
+
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
@@ -20,8 +27,8 @@ class Content(db.Model):
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Content(description='test', gender=1))
-    db.session.add(Content(description='test', gender=2))
+    db.session.add(Content(description='test', genger=Gender['MALE']))
+    db.session.add(Content(description='test', gender=Gender['MALE']))
 
     db.session.commit()
     lg.warning('Database initialized')
