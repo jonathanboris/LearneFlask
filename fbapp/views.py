dabcabdb27171430b9d02582c685f868f274a0f8
Jonathan OUEDREAOGO <jonathan.ouedreaogo@magellium.fr>
2023-05-10 12:40:59 +0200

    template block

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
@@ -7,4 +7,21 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+     description = """
+            Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
+                """
+     return render_template('index.html',
+                          user_name='Julio',
+                          user_image=url_for('static', filename='img/profile.png'),
+                          description=description,
+                          blur=True)
+
+@app.route('/result/')
+def result():
+      description = """
+            Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
+                """
+      return render_template('result.html',
+                          user_name='Tom',
+                          user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),
+                          description=description)
